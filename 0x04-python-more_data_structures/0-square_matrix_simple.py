5a67f972ec3bed79ca48fce08af8078def2bc99a
Gdija <khadijaghadi00@gmail.com>
2023-08-16 02:38:13 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         n_row = list([i**2 for i in row])
         copy_matrix.append(n_row)
-    return(copy_matrix)
+    return copy_matrix

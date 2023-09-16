d35a8aa2e65bba3db02a3af9dedb28e204d29df3
Gdija <khadijaghadi00@gmail.com>
2023-09-16 16:33:36 -0700

    create base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+class Base():
+    """create a base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            Base.id = Base.__nb_objects

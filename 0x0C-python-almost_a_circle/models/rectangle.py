a6af70ac8ed754d136e1153b0db31fa62b957dbd
Gdija <khadijaghadi00@gmail.com>
2023-09-18 16:38:41 -0700

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,14 +76,42 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """task5"""
-        for i in range(self.__width):
-            for i in range(self.__height):
-                print('#', end="")
-            print()
+        """task5 and 7"""
+        print("{}".format("\n"*self.y), end="")
+        for i in range(self.height):
+            print("{}{}".format(" "*self.x, "#"*self.width))
 
     def __str__(self):
         """task6"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """task 8 and 9"""
+        if args and len(args) != 0:
+            n = 0
+            for arg in args:
+                if n == 0:
+                    self.id = arg
+                elif n == 1:
+                    self.width = arg
+                elif n == 2:
+                    self.height = arg
+                elif n == 3:
+                    self.x = arg
+                elif n == 4:
+                    self.y = arg
+                n += 1
+        elif kwargs and len(kwargs):
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v

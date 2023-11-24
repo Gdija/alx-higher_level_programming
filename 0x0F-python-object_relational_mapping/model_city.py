91b1d3f17fc463a26e22f6056b908cbfbe256d94
Gdija <khadijaghadi00@gmail.com>
2023-11-24 13:40:41 -0800

    fix error

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,14 +4,14 @@ create a class in database
 '''
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+from model_state import Base, State
 
 Base = declarative_base()
 
 class City(Base):
     """city class for db"""
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, nullable=False, ForeignKey(State.id))
 

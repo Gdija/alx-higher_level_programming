c1960fa069769770752900cd3c42984d83704ad2
Gdija <khadijaghadi00@gmail.com>
2023-12-01 16:53:23 -0800

    http error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,12 +3,12 @@
 display the error code
 """
 import urllib.request
-import urllib.error
+from urllib.error import HTTPError
 import sys
 if __name__ == "__main__":
     request = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode('utf8'))
-    except urllib.error.HTTPError as error:
-        print('Error code: ', error.code)
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))

e9adae337786dec4902df62b29222eba60054243
Gdija <khadijaghadi00@gmail.com>
2024-01-03 15:59:17 -0800

    last tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (!err) {
+    return console.error(err);
+  }
+  const datas = JSON.parse(body);
+  const result = {};
+  for (const data of datas) {
+    if (data.completed) {
+      if (!result[data.userId]) {
+        result[data.userId] = 1;
+      } else {
+        result[data.userId]++;
+      }
+    }
+  }
+  console.log(result);
+});
